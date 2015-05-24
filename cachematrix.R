b484ff7ac89faeb8664a0e1d1b87769e661fae0d
lojacky <lojacky@rocketmail.com>
2015-05-24 18:49:37 +0800

    Update cachematrix.R
    1) Better variable naming
    2) Add comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,44 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
+#This function is to store the cache matrix and the related invertible matrix, 
+#You can retrieve the data by using $getMatrix, $getInvertibleMatrix and set the data by using $setMatrix(matrix), and $setInvertibleMatrix(matrix)
+
+# Assumption: The matrix is always invertible.
+#Input: Matrix
+#Output: The address of difference functions.
+
+makeCacheMatrix <- function(matrix_data = matrix()) {
+  invertibleMatix <- NULL
+  setMatrix <- function(y) {
+    matrix_data <<- y
+    invertibleMatix <<- NULL
   }
-  get <- function() x
-  setMatrix <- function(solve) m <<- solve
-  getMatrix <- function() m
-  list(set = set, get = get,
-       setMatrix = setMatrix,
-       getMatrix = getMatrix)
+  getMatrix <- function() matrix_data
+  setInvertibleMatrix <- function(solve) invertibleMatix <<- solve
+  getInvertibleMatrix <- function() invertibleMatix
+  list(setMatrix = setMatrix, getMatrix = getMatrix,
+       setInvertibleMatrix = setInvertibleMatrix,
+       getInvertibleMatrix = getInvertibleMatrix)
 }
 
 
-## Write a short comment describing this function
+
+# Assumption: The matrix is always invertible.
+#This function retrieve the invertible matrix if we have stored it. Otherwise, it will calculated the invertible matrix by using solve function.
+#Input: makeCacheMatrix function
+#Output: Invertible Matrix
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  m <- x$getMatrix()
+  m <- x$getInvertibleMatrix()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
-  data <- x$get()
+  data <- x$getMatrix()
   m <- solve(data, ...)
-  x$setMatrix(m)
+  x$setInvertibleMatrix(m)
   m
 }
